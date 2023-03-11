5e7f3522fce6d78c6e6e5bec177f92443a2c3468
Takumi Comeau <79553392+T3tsuo@users.noreply.github.com>
2023-03-11 19:55:23 -0400

    LevelFarming.py uses run function

--- LevelFarming.py
+++ LevelFarming.py
@@ -1,22 +1,19 @@
-import sys
 import os
 import time
 
 import fight
 import heal_return
 
-# this program grabs items and then fly's to go restore pokemon pp
+# this program xp levels pokemon
 
 os.environ['REQUESTS_CA_BUNDLE'] = "certifi/cacert.pem"
 
-try:
-    # try to grab input from command line for amount of sweet scent
-    x = int(sys.argv[1])
-except IndexError:
+
+def run():
     # else then ask for amount of times user can use sweet scent before going to pokecenter
     x = int(input("Number of times to use sweet scent: "))
-time.sleep(2)
-# loop forever
-while True:
-    heal_return.run()
-    fight.run(x)
+    time.sleep(2)
+    # loop forever
+    while True:
+        heal_return.run()
+        fight.run(x)
