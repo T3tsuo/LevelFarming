dbf0dda9acd5f7d212ccca4bd139cda2091ed778
Takumi Comeau <79553392+T3tsuo@users.noreply.github.com>
2023-03-11 22:28:28 -0400

    Update ToggleRun.py

--- ToggleRun.py
+++ ToggleRun.py
@@ -1,7 +1,9 @@
 import time
 import pydirectinput
 
-# time to switch to game
-time.sleep(2)
-# toggle run
-pydirectinput.press("x")
+def run():
+  input("Hit enter to toggle run and tab into game immediately: ")
+  # time to switch to game
+  time.sleep(2)
+  # toggle run
+  pydirectinput.press("x")
